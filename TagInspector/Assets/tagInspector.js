b254f5eca40b49d2cb8bfcdd0c6d0aaf521f83d0
Samir Ahmed <saahm@microsoft.com>
2015-08-15 22:06:07 -0700

    Initial searchbox, load, display html source
    add bootstrap, ace editor, jquery dependencies from CDN
    add custom styling in site.css
    add tagInspector.js for page logic
    
    fetch + display works

---
+++ TagInspector/Assets/tagInspector.js
@@ -0,0 +1,28 @@
+﻿function displayHtml(rawHtml) {
+    var prettyHtml = html_beautify(rawHtml);
+    editor.setValue(prettyHtml);
+}
+
+function triggerLoad() {
+    loadHtml($("#uri").val());
+}
+
+function loadHtml(url) {
+    $.getJSON("api/summary?url=" + encodeURI(url), function(data) {
+
+        if (data.failureReason) {
+            // failure
+        }
+        else {
+            displayHtml(data.body);
+        }
+    });
+}
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.setShowPrintMargin(false);
+editor.setReadOnly(true);
+editor.getSession().setMode("ace/mode/html");
+displayHtml("<html><body><p>Enter A Url To Inspect</p></body></html>");
+
